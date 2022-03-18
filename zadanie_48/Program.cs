4dc4f53320ed793e2f0217c04724126d18e93198
CreativeOZ <zagriaoleg@gmail.com>
2022-03-18 18:27:30 +0300

    48. Показать двумерный массив размером m×n заполненный целыми числами

---
+++ zadanie_48/Program.cs
@@ -0,0 +1,19 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+Console.Clear();
+
+Console.Write("Колличество срок: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Колличество стобцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int [,] array = new int [m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int z = 0; z < n; z++)
+    {
+        array[i,z] = new Random().Next(0,10);
+        Console.Write(array[i,z] + " ");
+    }
+    Console.WriteLine();
+}
